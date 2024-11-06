0407f6270eb52fac2e2880ae93a9bbf84886f07e
nailoth esdras <nailothn@gmail.com>
2024-11-06 02:19:52 +0100

    premier interfacfe, modification du fichiers scss utilities pour les brd-opacity

--- app/index.php
+++ app/index.php
@@ -1,8 +1,88 @@
 <?php require('ui/design-system/header.php'); ?>
 
-<header class="fixed lg:absolute inset-x-0 z-30 border-bottom border-white border-opacity-0.5 text-bg-white bg-opacity-100">
+<header class="fixed top-0 inset-x-0 z-30 brd-b brd-white brd-opacity-10 text-bg-white bg-opacity-100">
     <?php require('ui/components/annonce.php'); ?>
-    <!-- <?php  require('ui/components/navbar.php'); ?> -->
+    <?php  require('ui/components/navbar.php'); ?>
 </header>
+
+<div class="container relative bg-gradient-to-bl from-primary from-50 to-100 from-opacity-50" style="--ns-from-opacity: .35">
+    <div class="row mt-5 items-center py-3" style="--ns-mt-5: 8.5rem">
+        <div class="col-12 md:col-12 lg:col-7 xl:col-7 mx-auto order-2 md:order-1">
+            <div class="relative z-10 mx-auto px-5 pt-5 lg:px-5" style="--ns-pt-5: 5rem;">
+                <div class="mx-auto text-center lg:col-12">
+                    <div class="col-12">
+                        <h1 class="text-4xl sm:text-5xl lg:text-5xl xl:text-6xl fw-medium">
+                            <span class="sm:inline-block mb-5 sm:mb-5" style="--ns-sm-mb-5: 3rem">
+                                Créez des sites <span class="none lg:inline-block">Web</span>
+                            </span>
+                            <br>
+                            <span class="sm:inline-block mb-5 sm:mb-5" style="--ns-sm-mb-5: 3rem">
+                               rapides <span class="none xl:inline-block">et réactifs</span> avec
+                            </span>
+                            <br>
+                            
+                            <div class="mx-auto block w-max">
+                                <div class="relative block pb-2 mb-2">
+                                    <span class="absolute inset-0 z-10 block bg-clip-text bg-gradient-to-bl from-primary from-50 to-100 from-opacity-50">
+                                        twibiract
+                                    </span>
+                                    <span class="absolute inset-0 block  bg-clip-text bg-gradient-to-bl from-primary from-50 to-100 from-opacity-50">
+                                        twibiract
+                                    </span>
+                                    <span class="block">
+                                        twibiract
+                                    </span>
+                                </div>
+                                <div class="-mt-4 flex-grow overflow-hidden">
+                                    <svg class="w-60 sm:w-80" height="22" viewBox="0 0 283 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M1.24715 19.3744C72.4051 10.3594 228.122 -4.71194 281.724 7.12332" stroke="url(#paint0_linear_18_19)" stroke-width="4"></path>
+                                        <defs>
+                                            <linearGradient id="paint0_linear_18_19" x1="282" y1="5.49999" x2="40" y2="13" gradientUnits="userSpaceOnUse">
+                                                <stop stop-color="#00FFF0"></stop>
+                                                <stop offset="1" stop-color="#00A3FF"></stop>
+                                            </linearGradient>
+                                        </defs>
+                                    </svg>
+                                </div>
+                            </div>
+                        </h1>
+                        <p class="relative z-10 my-3 text-2xl fw-medium text-muted">
+                            Personnalisable · Puissante · Rapide · Joyeux
+                        </p>
+                        <div class="row items-center gap-1 md:gap-0 items-center">
+                            <a href="#" class="btn btn-primary col-6 md:col-3 mb-2 mx-auto">
+                                <span class="leading-none relative text-base tracking-wider text-white">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-book-half" viewBox="0 0 16 16">
+                                        <path d="M8.5 2.687c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783"/>
+                                    </svg>
+                                    Commencer
+                                </span>
+                            </a>
+                            <a href="#" class="btn btn-dark col-auto md:col-4 mb-2 mx-auto">
+                                <span class="leading-none relative text-base tracking-wider text-white">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-folder-fill" viewBox="0 0 16 16">
+                                        <path d="M9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.825a2 2 0 0 1-1.991-1.819l-.637-7a2 2 0 0 1 .342-1.31L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3m-8.322.12q.322-.119.684-.12h5.396l-.707-.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981z"/>
+                                    </svg>
+                                    Documents <span class="none md:inline-block">interactifs</span>
+                                </span>
+                            </a>
+                            <a href="#" class="col-12 md:col-3">
+                                <span class="duration-300 group-hover:tracking-wider group-hover:underline group-hover:underline-offset-2">
+                                    Tutoriel
+                                </span>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-4 w-4 translate-y-px duration-300 group-hover:translate-x-1">
+                                    <path fill-rule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd"></path>
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 md:col-4 lg:col-5 xl:col-5 mx-auto order-1 md:order-2 text-center">
+            <img src="../svg/twibiract.v1.svg" alt="twibiract.svg" class="relative z-10 col-8 md:col-12 mx-auto pt-5 lg:px-5" style="--ns-pt-5: 5rem;">
+        </div>
+    </div>
+</div>
 
 <?php require('ui/design-system/footer.php'); ?>
--- app/ui/components/navbar.php
+++ app/ui/components/navbar.php
@@ -1,6 +1,6 @@
-<nav class="navbar lg:navbar-expand backdrop-blur shadow-sm shadow-dark p-3 ">
+<nav class="navbar lg:navbar-expand backdrop-blur shadow-sm shadow-dark p-3">
     <div class="container-fluid">
-        <a class="navbar-brand flex items-center" href="#">
+        <a class="navbar-brand flex items-center" href="index.php">
             <img src="../svg/twibiract.v1.svg" class="img-fluid" width="36px" height="36px">
             <div class="h4 mb-0">twibiract</div>
         </a>
@@ -12,25 +12,25 @@
             <ul class="navbar-nav ml-auto mb-0 mb-lg-0">
                 
                 <li class="nav-item">
-                    <a class="nav-link px-5" href="#">
+                    <a class="nav-link px-4" href="guide.php">
                         Guide
                     </a>
                 </li>
 
                 <li class="nav-item">
-                    <a class="nav-link px-5" href="#">
+                    <a class="nav-link px-4" href="themes.php">
                         Thèmes
                     </a>
                 </li>
 
                 <li class="nav-item">
-                    <a class="nav-link px-5" href="#">
+                    <a class="nav-link px-4" href="exemples.php">
                         Exemples
                     </a>
                 </li>
                 
                 <li class="nav-item">
-                    <a class="nav-link px-5" href="#">
+                    <a class="nav-link px-4" href="extrait.php">
                         Extraits
                     </a>
                 </li>
@@ -38,7 +38,7 @@
                 <div class="none lg:flex items-center">
                                     
                     <li class="nav-item">
-                        <a class="nav-link border-left" href="https://github.com/twibiractlabs" target="_blank">
+                        <a class="nav-link brd-l" href="https://github.com/twibiractlabs" target="_blank">
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
                                 <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
                             </svg>
@@ -46,7 +46,7 @@
                     </li>
                     
                     <li class="nav-item">
-                        <a class="nav-link border-right" href="https://x.com/N95524" target="_blank">
+                        <a class="nav-link brd-r" href="https://x.com/N95524" target="_blank">
                             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-twitter-x" viewBox="0 0 16 16">
                                 <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z"/>
                             </svg>
@@ -54,7 +54,7 @@
                     </li>
                     
                     <li class="nav-item">
-                        <a class="nav-link border-right" href="https://x.com/N95524" target="_blank">
+                        <a class="nav-link brd-r" href="https://x.com/N95524" target="_blank">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-brightness-low-fill" viewBox="0 0 16 16">
                                 <path d="M12 8a4 4 0 1 1-8 0 4 4 0 0 1 8 0M8.5 2.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0m0 11a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0m5-5a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1m-11 0a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1m9.743-4.036a.5.5 0 1 1-.707-.707.5.5 0 0 1 .707.707m-7.779 7.779a.5.5 0 1 1-.707-.707.5.5 0 0 1 .707.707m7.072 0a.5.5 0 1 1 .707-.707.5.5 0 0 1-.707.707M3.757 4.464a.5.5 0 1 1 .707-.707.5.5 0 0 1-.707.707"/>
                             </svg>
